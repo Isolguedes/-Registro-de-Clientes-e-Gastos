b39b45f637c18cf34be53f8ed84a321a84da66df
Sophia Custódio <Solguedesdaluz@gmail.com>
2025-06-09 21:03:29 -0300

    Add files via upload

--- atividade 4.py
+++ atividade 4.py
@@ -1,9 +1,8 @@
 def main():
     clientes = []
-
+    
     while True:
         nome = input("DIGITE O NOME DO CLIENTE: ")
-
         while True:
             vgastoinput = input("DIGITE O VALOR GASTO EM REAIS: R$ ")
             if vgastoinput.strip().isdigit():
@@ -21,15 +20,14 @@ def main():
             break
 
     total = len(clientes)
-    if total > 0:
-        soma = sum(p[1] for p in clientes)
-        clientemaismais = max(clientes, key=lambda p: p[1])
+    soma = sum(p[1] for p in clientes)
+    clientemaismais = max(clientes, key=lambda p: p[1])
 
-        print("\nRESULTADOS FINAIS:")
-        print(f"Total de clientes cadastrados: {total}")
-        print(f"Total gasto: R$ {soma}")
-        print(f"O cliente que mais comprou é: {clientemaismais}")
+    print("\nRESULTADOS FINAIS:")
+    print(f"Total de clientes cadastrados: {total}")
+    print(f"Total gasto: R$ {soma}")
+    print(f"O cliente que mais comprou é: {clientemaismais}")
 
 
-if __name__ == "__Main__":
+if __name__ == "__main__":
     main()
